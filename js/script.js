286247d79f7a1f005303481ee7f76285ca2f671e
Your Name <fpasamuelmayowa51@gmail.com>
2024-12-16 02:59:16 +0100

    update

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@
     "use strict";
     $(document).ready(function () {
         ssPreloader();
+        navShow();
+        hideHeader();
+        textAreaCheck();
     });
     const ssPreloader = function () {
         $("html").addClass('ss-preload');
@@ -13,4 +16,45 @@
             $("html").addClass('ss-loaded');
         });
     };
+    const navShow = function () {
+        $('#checkbox').change(function() {
+            if ($(this).is(':checked')) {
+              $('.myHeader').addClass("show");
+            } else {
+              $('.myHeader').removeClass("show");
+            }
+        });
+        // $('#checkbox').change(function() {
+        //     $('.myHeader').toggleClass("show", $(this).is(':checked'));
+        // });
+    }
+
+    const hideHeader = function () {
+        let lastScrollTop = 0;
+        $(window).on('scroll', function() {
+            let currentScroll = $(this).scrollTop();
+            if (currentScroll > lastScrollTop) {
+            // Scrolling down
+            $('.myHeader').addClass('hide');
+            } else {
+            // Scrolling up
+            $('.myHeader').removeClass('hide');
+            }
+            lastScrollTop = currentScroll;
+        });
+    }
+    const textAreaCheck = function (){
+        const maxWords = 300;
+
+    $('#lined-textarea').on('input', function () {
+        const text = $(this).val();
+        const words = text.trim().split(/\s+/);
+        const wordCount = words.filter(word => word.length > 0).length;
+        if (wordCount > maxWords) {
+            const truncatedWords = words.slice(0, maxWords).join(' ');
+            $(this).val(truncatedWords);
+        }
+        $('#word-count').text(`Words: ${Math.min(wordCount, maxWords)}/${maxWords}`);
+    });
+    }
 })(jQuery);
